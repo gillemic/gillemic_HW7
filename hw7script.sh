ea92bfa338fd873a3db0ba0357175bb605af869a
Michael Gillett <gillemic@oregonstate.edu>
2019-05-20 23:58:12 -0700

    Add files via upload

---
+++ hw7script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ -e ~/.ssh/id_rsa.pub ]
+then
+	echo -e "\e[1;36mSkipping ssh key generation (file exists).\e[0m"
+	echo
+	echo
+	sleep 3
+else
+	echo -e "\e[1;32mGenerating ssh key\e[0m"
+	ssh-keygen -t rsa 
+	$not_my_first_time=1
+fi
+
+echo -e "\e[1;32mCopying the ssh key to Alpine VMs\e[0m"
+ssh-copy-id root@192.168.1.20
+ssh-copy-id root@192.168.1.21
+ssh-copy-id root@192.168.1.22
+ssh-copy-id root@192.168.1.23
+
+echo -e "\e[1;32mConfiguring Ansible Playbook\e[0m"
+ansible-playbook webserver.yaml -i hosts.ini
+
+echo -e "\e[1;32mCurling...\e[0m"
+curl 192.168.1.20
+sleep 2
+curl 192.168.1.21
+sleep 2
+curl 192.168.1.22
+sleep 2
+curl 192.168.1.23
+
